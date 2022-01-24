1265112bf9ccd2690b48370a66ec15a78dfd6791
FelixJacobsen <felix.jacobsen@iths.se>
2022-01-24 20:32:46 +0100

    Add test to test exception handling in payEmployees method

--- src/test/java/com/example/EmployeeManagerTest.java
+++ src/test/java/com/example/EmployeeManagerTest.java
@@ -3,6 +3,9 @@ package com.example;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.anyDouble;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 
 class EmployeeManagerTest {
@@ -17,5 +20,11 @@ class EmployeeManagerTest {
         assertThat(result).isEqualTo(2);
     }
 
+    @Test
+    void throwingARuntimeExceptionShouldSetIsPaidToFalse() {
+        doThrow(RuntimeException.class).when(bankService).pay(anyString(), anyDouble());
+        employeeManager.payEmployees();
+        assertThat(employeeRepositoryMock.findAll().get(0).isPaid()).isFalse();
+    }
 
 }
