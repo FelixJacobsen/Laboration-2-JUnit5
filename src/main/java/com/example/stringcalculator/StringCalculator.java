5639c71e3d9c0abbd53eb08304be2c54fe151837
FelixJacobsen <felix.jacobsen@iths.se>
2022-01-24 17:38:05 +0100

    Add function and test to prevent negative numbers

--- src/main/java/com/example/stringcalculator/StringCalculator.java
+++ src/main/java/com/example/stringcalculator/StringCalculator.java
@@ -1,5 +1,8 @@
 package com.example.stringcalculator;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class StringCalculator {
@@ -13,7 +16,26 @@ public class StringCalculator {
         if (numbers.startsWith("//"))
             delimiter = numbers.charAt(2) + "|\n";
 
+        if(numbers.contains("-"))
+            filterNegatives(numbers);
+
+
         return getSum(numbers, delimiter);
+    }
+
+    private IntStream getNumbers(String numbers){
+        return Arrays.stream(numbers.split(delimiter))
+                .mapToInt(Integer::parseInt);
+    }
+
+    private void filterNegatives(String numbers) {
+        String negatives = getNumbers(numbers)
+                .filter(n -> n < 0)
+                .mapToObj(Integer::toString)
+                .collect(Collectors.joining(","));
+                if(!negatives.isEmpty())
+                    throw new IllegalArgumentException("Negative numbers is not allowed!" +
+                            " Numbers:" + negatives);
     }
 
     private int getSum(String numbers, String delimiter) {
--- src/test/java/com/example/stringcalculator/StringCalculatorTest.java
+++ src/test/java/com/example/stringcalculator/StringCalculatorTest.java
@@ -3,6 +3,7 @@ package com.example.stringcalculator;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class StringCalculatorTest {
     StringCalculator stringCalculator = new StringCalculator();
@@ -25,6 +26,12 @@ public class StringCalculatorTest {
     @Test
     void differentDelimiterShouldReturn8(){
         assertThat(stringCalculator.add("//;\n5;3")).isEqualTo(8);
+    }
+
+    @Test
+    void negativeNumbersThrowAExceptionAndShowIllegalNumbers(){
+        assertThatThrownBy(() -> stringCalculator.add("-3,-5"))
+                .hasMessageContaining("Negative numbers is not allowed! Numbers:-3,-5");
     }
 
 }
